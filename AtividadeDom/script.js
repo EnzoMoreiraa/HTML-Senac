b8499b77c1920c001958ade2a19a59fad324ae45
enzo moreira <enzomoreiradasilveira@gmail.com>
2024-11-06 09:50:20 -0300

    new changes

--- AtividadeDom/script.js
+++ AtividadeDom/script.js
@@ -1,30 +1,68 @@
+/* Atividade 01 */
+
 let square1 = document.querySelector(`.square1`)
 let square2 = document.querySelector(`.square2`)
 let square3 = document.querySelector(`.square3`)
 
 square1.addEventListener(`click`, () => {
-    if (square1.style.backgroundColor === 'purple') {
-        square1.style.backgroundColor = 'blue';
-    } else {
-        square1.style.backgroundColor = 'purple';
-    }
+    square1.style.backgroundColor = square1.style.backgroundColor == 'purple' ? 'blue' : 'purple';
+    //toma os operador dele
 }
 )
 
 square2.addEventListener(`click`, () => {
-    if (square2.style.backgroundColor === 'pink') {
-        square2.style.backgroundColor = 'red';
-    } else {
-        square2.style.backgroundColor = 'pink';
-    }
+    square2.style.backgroundColor = square2.style.backgroundColor == 'pink' ? 'red' : 'pink'
 }
 )
 
 square3.addEventListener(`click`, () => {
-    if (square3.style.backgroundColor === 'green') {
-        square3.style.backgroundColor = 'yellow';
-    } else {
-        square3.style.backgroundColor = 'green';
-    }
+    square3.style.backgroundColor = square3.style.backgroundColor == 'green' ? 'yellow' : 'green'
 }
 )
+
+/* Atividade 02 */
+
+let click = document.querySelector('.Click')
+let reset = document.querySelector('.Reset')
+let cc = document.querySelector('.num')
+let i = 0
+
+click.addEventListener('click', () => {
+    i++;
+    cc.innerHTML = i
+})
+
+reset.addEventListener('click', () => {
+    i = 0
+    cc.innerHTML = i
+    
+})
+
+/* Atividade 03 */
+
+let text = document.querySelector('.Text')
+let button = document.querySelector('.button')
+
+button.addEventListener('click', () => {
+    text.innerHTML = text.innerHTML == '' ? "Cidade Casca de Bill Amostradinho" : ''
+})
+
+/* Atividade 04 */
+
+let change = document.querySelector('.Change')
+
+let textChanged = document.querySelector('.textChanged')
+
+change.addEventListener('click', () => {
+    let insert = prompt('Insira um texto')
+    textChanged.innerHTML = insert
+    
+})
+
+/* Atividade 05 */
+
+let tecla = document.querySelector('.tecla')
+
+document.addEventListener('keydown', (press) => {
+    tecla.innerHTML = `Você precionou <br> - ${press.key} -`
+})
